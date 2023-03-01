1a1cdc4ef6028d9af14f2de0b80b95c498923fe7
Justin Storfinger <info@jstorfinger.de>
2023-03-01 12:24:46 +0100

    Replace bootstrap with tabler Add complete tabler library Add template page

---
+++ public/auth.php
@@ -0,0 +1,11 @@
+<?php
+    /*
+        Copyright (C) 2023 - Justin Storfinger - All Rights Reserved.
+        This file is part of the simple asset management platform.
+    */
+
+
+
+
+
+?>
---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+    /*
+        Copyright (C) 2023 - Justin Storfinger - All Rights Reserved.
+        This file is part of the simple asset management platform.
+    */
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title></title>
+    </head>
+</html>
